82a61c3d0c4db93e3a03c03720e460ed8c897a52
Muhammad Awan <muhammadawan434@gmail.com>
2024-10-10 19:22:43 -0400

    Added tab icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 export const metadata: Metadata = {
   title: "Muhammad Awan",
   description: "Welcome to my portfolio website",
+  icons: {
+    icon: '/favicon.svg', 
+  },
 };
 
 export default function RootLayout({
